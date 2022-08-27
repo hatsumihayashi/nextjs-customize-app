57ca4ccda68b59df87093f8f146ecc285a4c9d56
Hatsumi Hayashi <hayashihatsumi0114@gmail.com>
2022-08-27 17:53:58 +0900

    Postを一覧で表示できる`/blog`ページを追加

--- components/Post.tsx
+++ components/Post.tsx
@@ -7,3 +7,16 @@ export type Post = {
   content: string
   published: boolean
 }
+
+export const PostCard: React.FC<{ post: Post }> = ({ post }) => {
+  return (
+    <div className="border-gray-500 border-2 p-4 my-2">
+      <div className="text-xl">{post.title}</div>
+      <div className="flex flex-col">
+        <p className="text-sm">{post.author?.name}</p>
+        {/** TODO: 日付を出せると良さそう */}
+      </div>
+      <div className="truncate">{post.content}</div>
+    </div>
+  )
+}
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import type { GetStaticProps } from 'next'
+import { Post, PostCard } from '../components/Post'
 
 export const getStaticProps: GetStaticProps = async () => {
   const prisma = new PrismaClient()
@@ -16,3 +17,18 @@ export const getStaticProps: GetStaticProps = async () => {
     revalidate: 10,
   }
 }
+
+const Blog: React.FC<{ posts: Post[] }> = ({ posts }) => {
+  return (
+    <main className="m-6">
+      <div className="text-2xl">ブログ一覧</div>
+      <div className="mt-6">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+    </main>
+  )
+}
+
+export default Blog
